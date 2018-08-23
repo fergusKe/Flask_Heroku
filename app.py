695dc3de1d225bd3d1d170aa3a673cc0634d2685
Fergus <fergus.jingtai@gmail.com>
2018-08-23 16:39:58 +0800

    update

--- app.py
+++ app.py
@@ -5,51 +5,17 @@ Created on Sat Aug 18 01:00:17 2018
 @author: linzino
 """
 
-
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-# Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
-# Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
-
 @app.route("/")
+def hello():
+    return render_template('index.html')
+
+#新分頁
+@app.route("/test/path")
 def new_Path():
-    return "Hello World"
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
- 
+    return "Hello World /test/path!"
 
 if __name__ == '__main__':
     app.run(debug=True)
