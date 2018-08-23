1fdfcca7406d5bd09be73b5cd4a5ae14fe1a79d0
Fergus <fergus.jingtai@gmail.com>
2018-08-23 16:46:02 +0800

    update

--- app.py
+++ app.py
@@ -1,21 +1,47 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 18 01:00:17 2018
+from flask import Flask, request, abort
 
-@author: linzino
-"""
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
-from flask import Flask, render_template
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('DVhOUDsodBZKg5Aiolyox+mPtB4/O4Gsuo9eh9B1c7Z9r2u4g/3tNxoXCEA8uKKYE6NyVXAPWbXd9HzIsLOg/RREFYBKzKJAm1rM2bewgGAe991grswCSOFW89niqywqZIr+c6o2z7ZHtBKSHIxr0QdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('ec95ecb53834da2fbd5b9f2928263a50')
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-#新分頁
-@app.route("/test/path")
-def new_Path():
-    return "Hello World /test/path!"
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
 
 if __name__ == '__main__':
     app.run(debug=True)
