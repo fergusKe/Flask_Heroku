df6ad8f9253d8567d5a1cec1f0a4c9183cd8fba0
Fergus <fergus.jingtai@gmail.com>
2018-08-23 16:19:49 +0800

    update

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug 18 01:00:17 2018
-
-@author: linzino
-"""
-
-
 from flask import Flask, request, abort
 
 from linebot import (
@@ -14,43 +6,37 @@ from linebot import (
 from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from linebot.models import *
 
 app = Flask(__name__)
 
+# Channel Access Token
 line_bot_api = LineBotApi('DVhOUDsodBZKg5Aiolyox+mPtB4/O4Gsuo9eh9B1c7Z9r2u4g/3tNxoXCEA8uKKYE6NyVXAPWbXd9HzIsLOg/RREFYBKzKJAm1rM2bewgGAe991grswCSOFW89niqywqZIr+c6o2z7ZHtBKSHIxr0QdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
 handler = WebhookHandler('ec95ecb53834da2fbd5b9f2928263a50')
 
-
-
+# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-
-    
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
 
-
+# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
- 
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(event.reply_token, message)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
